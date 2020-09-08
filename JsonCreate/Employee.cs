eadca7ef6dc14154e2529e732985c5543d54980e
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-09-08 15:22:09 +0200

    Lab 8 fixes the bug

---
+++ JsonCreate/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonCreate
+{
+    class Employee
+    {
+        public string FirstName = "zahra";
+     public string LastName = "Abdoli";
+        public int EmployeeID = 10987654;
+        public string Designation = "Teacher";
+    }
+}
